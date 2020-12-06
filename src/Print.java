37041a5053cf1e1bd28c96c0af1e5b379eda7976
jw1028 <1711982870@qq.com>
2020-12-06 22:23:11 +0800

    递归打印数字的每一位

--- src/Print.java
+++ src/Print.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Print {
+    public static void Print(int n){
+        if(n>9)
+        {
+            Print(n/10);
+        }
+        System.out.println(n%10);
+
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        Print(n);
+    }
 }
