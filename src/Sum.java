274c4bafce9f483a9c629b92d0a802c0b147d99e
jw1028 <1711982870@qq.com>
2020-12-06 22:47:12 +0800

    写一个递归方法，输入一个非负整数，返回组成它的数字之和

--- src/Sum.java
+++ src/Sum.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Sum {
+    public static int Sum(int n){
+        if(n<9)
+        {
+            return n%10;
+        }else{
+            return n%10+Sum(n/10);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int ret=Sum(n);
+        System.out.println(ret);
+    }
+
 }
