f707624337e18fc68cc8430ee3f60723e1667ad9
jw1028 <1711982870@qq.com>
2020-12-08 23:31:07 +0800

    汉诺塔

--- src/Hanoi.java
+++ src/Hanoi.java
@@ -1,11 +1,28 @@
-package PACKAGE_NAME;
 
-/**
- * Created with Intellig IDEA
- * Description:
- * User: zjc
- * Date: 2020- 12 -08
- * Time: 22:55
- */
+import java.util.Scanner;
 public class Hanoi {
+    public static void move(char pos1,char pos2) {
+
+        System.out.print(pos1+"->"+pos2 +" ");
+    }
+
+    public static void Hanoi(int n,char pos1, char pos2,char pos3) {
+        if(n == 1) {
+            move(pos1,pos3);
+            return;
+        }else {
+            Hanoi(n-1,pos1,pos3,pos2);
+            move(pos1,pos3);
+            Hanoi(n-1,pos2,pos1,pos3);
+        }
+    }
+
+    public static void main1(String[] args) {
+        /*Hanoi(1,'A','B','C');
+        System.out.println();
+        Hanoi(2,'A','B','C');
+        System.out.println();*/
+        Hanoi(64,'A','B','C');
+        System.out.println();
+    }
 }
