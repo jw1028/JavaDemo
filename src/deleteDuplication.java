094626e6484077283e02ced7f6f5d5ffd21a3d8b
jw1028 <1711982870@qq.com>
2021-01-05 11:32:23 +0800

    剑指offer删除链表中的重复节点

--- src/deleteDuplication.java
+++ src/deleteDuplication.java
@@ -1,11 +1,27 @@
-package PACKAGE_NAME;
-
-/**
- * Created with Intellig IDEA
- * Description:
- * User: zjc
- * Date: 2021- 01 -05
- * Time: 11:26
- */
-public class deleteDuplication {
+public class Solution {
+    public ListNode deleteDuplication(ListNode pHead)
+    {
+        if(pHead == null)
+        {
+            return null;
+        }
+        ListNode newHead = new ListNode(-1);
+        ListNode tmp = newHead;
+        ListNode cur = pHead;
+        while(cur != null) {
+            if(cur.next!=null&&cur.val == cur.next.val) {
+                while(cur.next!=null&&cur.val==cur.next.val)
+                {
+                    cur=cur.next;
+                }
+                cur=cur.next;
+            }else{
+                tmp.next=cur;
+                tmp=tmp.next;
+                cur=cur.next;
+            }
+            tmp.next=null;
+        }
+        return newHead.next;
+    }
 }
