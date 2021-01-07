bda737aba31a9bb04420ad864b521f4a97b5d1a7
jw1028 <1711982870@qq.com>
2021-01-07 18:19:44 +0800

    LeetCode160相交链表

--- LeetCode/src/getIntersectionNode.java
+++ LeetCode/src/getIntersectionNode.java
@@ -1,11 +1,46 @@
-package PACKAGE_NAME;
-
-/**
- * Created with Intellig IDEA
- * Description:
- * User: zjc
- * Date: 2021- 01 -07
- * Time: 18:16
- */
-public class getIntersectionNode {
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null||headB==null)
+        {
+            return null;
+        }
+        //假设链表A长
+        ListNode pl=headA;
+        ListNode ps=headB;
+        int lenA=0;
+        int lenB=0;
+        //求链表AB的长度
+        while(pl!=null)
+        {
+            pl=pl.next;
+            lenA++;
+        }
+        while(ps!=null)
+        {
+            ps=ps.next;
+            lenB++;
+        }
+        pl=headA;
+        ps=headB;
+        //求差值
+        int k=lenA-lenB;
+        if(k<0)
+        {
+            pl=headB;
+            ps=headA;
+            k=lenB-lenA;
+        }
+        while(k!=0)
+        {
+            pl=pl.next;
+            k--;
+        }
+        //判断是否相等
+        while(pl!=ps)
+        {
+            pl=pl.next;
+            ps=ps.next;
+        }
+        return pl;
+    }
 }
