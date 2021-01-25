7bc6697e7356cf90f17313aaab55d3d4c692b4e9
jw1028 <1711982870@qq.com>
2021-01-25 21:50:21 +0800

    155最小栈

---
+++ LeetCode/src/MinStack.java
@@ -0,0 +1,36 @@
+class MinStack {
+    public Stack<Integer> stack=new Stack();
+    public Stack<Integer> minStack=new Stack();
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }  
+    public void push(int x) {
+      stack.push(x);
+      if(minStack.empty())
+      {
+          minStack.push(x);
+      }else{
+          if(x<=minStack.peek())
+          {
+              minStack.push(x);
+          }
+      }
+    }
+    
+    public void pop() {
+     
+        if(stack.pop().equals(minStack.peek()))
+        {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+   return minStack.peek();
+    }
+}
