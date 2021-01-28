981b1622ca914b685595bdc036d4e070a53c01b5
jw1028 <1711982870@qq.com>
2021-01-28 08:54:02 +0800

    剑指offer55二叉树的深度

---
+++ LeetCode/src/maxDepth.java
@@ -0,0 +1,8 @@
+class Solution {
+     public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return leftHeight  > rightHeight  ?  leftHeight +1 : rightHeight +1;
+    }
+}
