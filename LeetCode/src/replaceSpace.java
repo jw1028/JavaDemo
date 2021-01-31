b735f48f86fd2548311d3b998e6bc6b7d41411d8
jw1028 <1711982870@qq.com>
2021-01-31 15:09:14 +0800

    剑指offer05替换空格

---
+++ LeetCode/src/replaceSpace.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String replaceSpace(String s) {
+         StringBuilder sb=new StringBuilder();
+         for(int i=0;i<s.length();i++)
+         {
+             char ch=s.charAt(i);
+             if(ch==' ')
+             {
+                 sb.append("%20");
+             }else{
+             sb.append(ch);
+         }
+         }
+         return sb.toString();
+    }
+}
