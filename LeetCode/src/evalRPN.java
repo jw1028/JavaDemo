eb26d08a619707977867aabec1b5336199a7fecf
jw1028 <1711982870@qq.com>
2021-02-18 20:04:40 +0800

    LeetCode150逆波兰表达式

---
+++ LeetCode/src/evalRPN.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/") ) {
+                int a = Integer.valueOf(stack.pop());
+                int b = Integer.valueOf(stack.pop());
+                if (tokens[i].equals("+"))
+                    stack.push(b + a);
+                if (tokens[i].equals("-"))
+                    stack.push(b - a);
+                if (tokens[i].equals("*"))
+                    stack.push(b * a);
+                if (tokens[i].equals("/"))
+                    stack.push(b / a);
+            }
+            else {
+                stack.push(Integer.valueOf(tokens[i]));
+            }                                
+        }
+        return stack.pop();
+    }
+}
