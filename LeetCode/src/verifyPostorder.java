5802aeaa78c9feaed54cc0c0ea0008c5b7358430
jw1028 <1711982870@qq.com>
2021-02-21 17:25:14 +0800

    剑指offer33二叉搜索树的后序遍历

--- LeetCode/src/verifyPostorder.java
+++ LeetCode/src/verifyPostorder.java
@@ -1,13 +1,17 @@
+
 class Solution {
     public boolean verifyPostorder(int[] postorder) {
-        return recur(postorder, 0, postorder.length - 1);
+        return dfs(postorder, 0, postorder.length - 1);
     }
-    boolean recur(int[] postorder, int i, int j) {
-        if(i >= j) return true;
-        int p = i;
-        while(postorder[p] < postorder[j]) p++;
-        int m = p;
-        while(postorder[p] > postorder[j]) p++;
-        return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
+    boolean dfs(int[] postorder, int left, int right) {
+        if(left >= right) return true;
+        int root=postorder[right];
+        int k = left;
+        while(k<right&&postorder[k] < root) k++;
+        for(int i=k;i<right;i++)
+        {
+            if(postorder[i]<root) return false;
+        }
+        return  dfs(postorder, left, k - 1) && dfs(postorder, k, right-1);
     }
 }
