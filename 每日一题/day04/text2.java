81229a61fc61d3ab6b87dc65cfb61881acdd6526
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-04-20 20:12:06 +0800

    进制转换

---
+++ 每日一题/day04/text2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Integer M = scan.nextInt();
+        Integer N = scan.nextInt();
+        System.out.print(Integer.toString(M, N));
+    }
+}
