ebfd27447e6728807b6524081568fbb1c3ab60f7
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-04-20 20:09:41 +0800

    字符串中找出连续最长的数字串

--- 每日一题/day03/text1.java
+++ 每日一题/day03/text1.java
@@ -1,1 +1,24 @@
-
+import java.util.*;
+public class Main{
+    public static String longStr(String str) {
+        int max = 0, cnt = 0, end = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                cnt++;
+                if(max < cnt) {
+                    max = cnt;
+                    end = i;
+                }
+            }else {
+                cnt = 0;
+            }
+        }
+        return str.substring(end - max + 1, end + 1);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        String ret = longStr(s);
+        System.out.print(ret);
+    }
+}
