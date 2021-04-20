d0b2bf88352f8b98d360d22024388867186afec6
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-04-20 19:59:55 +0800

    倒置字符串

--- 每日一题/day02/text1.java
+++ 每日一题/day02/text1.java
@@ -1,1 +1,17 @@
-21
+
+import java.util.*;
+public class Main{
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+String str = scan.nextLine();
+StringBuilder ret = new StringBuilder();
+int j = str.length() - 1, i = j;
+while(i >= 0) {
+while(i >= 0 && str.charAt(i) != ' ') i--;
+ret.append(str.substring(i + 1, j + 1) + " ");
+while(i >= 0 && str.charAt(i) == ' ') i--;
+j = i;
+}
+System.out.print(ret.toString().trim());
+}
+}
