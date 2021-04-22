edd278ad01cd62f5b3812082623e9cf3aaeb3b69
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-04-22 22:48:52 +0800

    求最小公倍数

--- 每日一题/day07/text1.java
+++ 每日一题/day07/text1.java
@@ -1,1 +1,15 @@
-dfsa
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(), m = scan.nextInt();
+        int ret = n > m ? n : m;
+        while(true) {
+            if(ret % n ==0 && ret % m == 0) {
+                System.out.print(ret);
+                break;
+            }
+            ret++;
+        }
+    }
+}
