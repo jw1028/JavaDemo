8c10e05062a9d1aaedbfa298f007161ddd77511e
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-04-23 20:40:14 +0800

    查找组成一个偶数最接近的两个素数

--- 每日一题/day10/text1.java
+++ 每日一题/day10/text1.java
@@ -1,1 +1,27 @@
-
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            int n = scan.nextInt();
+            int[] res = new int[2];
+            for(int i = 1;i <= n/2;i++){
+                if(isPrime(i) && isPrime(n-i)){
+                    res[0] = i;
+                    res[1] = n-i;
+                }
+            }
+            System.out.println(res[0]);
+            System.out.println(res[1]);
+        }
+        scan.close();
+    }
+    
+    private static boolean isPrime(int num){
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
