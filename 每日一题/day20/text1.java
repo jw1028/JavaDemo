e2483d6ca0df3f551ce4f9523ed8b1773c272327
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-05-11 22:41:44 +0800

    反转字符串

--- 每日一题/day20/text1.java
+++ 每日一题/day20/text1.java
@@ -1,1 +1,16 @@
-sa
+import java.util.*;
+
+
+public class Solution {
+   
+    public String solve (String str) {
+        // write code here
+        char[] s=str.toCharArray();
+        char[] res=new char[s.length];
+        int j=0;
+        for(int i=s.length-1;i>=0;i--){
+            res[j++]=s[i];
+        }
+        return String.valueOf(res);
+    }
+}
