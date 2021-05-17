b0040530715ede1288ccfe6ddcb0d5c4b5e2763d
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-05-17 23:47:11 +0800

    变态青蛙跳台阶

--- 每日一题/day25/text1.java
+++ 每日一题/day25/text1.java
@@ -1,1 +1,13 @@
-dsa
+public class Solution {
+    public int jumpFloorII(int n) {
+        if(n < 2) {
+            return 1;
+        }
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            arr[i] = 2 * arr[i - 1];
+        }
+        return arr[n];
+    }
+}
