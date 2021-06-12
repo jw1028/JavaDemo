4159f3d083b58ef58b20e98a3aa517141eb47c3f
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-06-12 23:41:10 +0800

    数组中次数超过一半的数字

--- 每日一题/day34/text1.java
+++ 每日一题/day34/text1.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    public int majorityElement(int[] nums) {
+        int val = 0, count = 0;
+        for(int x : nums){
+            if(count == 0)
+            {
+               val=x;
+               count=1;
+            }else{
+                if(val==x)
+                {
+                    count++;
+                }else{
+                    count--;
+                }
+            }
+        }
+        return val;
+    }
+}
