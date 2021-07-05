b0b765b896fa6343859f544c9b93660e51dd3ecd
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-05 23:34:42 +0800

    LeetCode2两数相加

---
+++ src/addTwoNumbers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode head = null, tail = null;
+        int carray = 0;
+        while(l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carray;
+            if(head == null) {
+                head = tail = new ListNode(sum % 10);
+            }else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carray = sum /10;
+            if(l1 != null) {
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if(carray > 0) {
+            tail.next = new ListNode(carray);
+        }
+        return head;
+        
+    }
+}
