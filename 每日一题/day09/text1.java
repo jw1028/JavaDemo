4483ac1c2bb1119ad690afd06c833530a3ccccac
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-12 22:10:07 +0800

    重建二叉树

--- 每日一题/day09/text1.java
+++ 每日一题/day09/text1.java
@@ -1,1 +1,29 @@
-dsa
+public class Solution {
+    public int preIndex = 0;
+    public TreeNode dfs(int[] pre, int[] in, int l, int r) {
+        if(l > r) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[preIndex]);
+        int index = findVal(in, pre[preIndex], l, r);
+        preIndex++;
+        root.left = dfs(pre, in, l, index - 1);
+        root.right = dfs(pre, in, index + 1, r);
+        return root;
+        
+    }
+    public int findVal(int[] in, int key, int l, int r) {
+        for(int i = l; i <= r; i++) {
+            if(key == in[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        if(pre == null || in == null) {
+            return null;
+        }
+        return dfs(pre, in, 0, in.length - 1);
+    }
+}
