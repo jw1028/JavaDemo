15e96b27c92604354a6b6a5c9f973c99c13d34e0
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-13 19:37:05 +0800

    无重复最长子串

--- 每日一题/day11/text1.java
+++ 每日一题/day11/text1.java
@@ -1,1 +1,19 @@
-dsa
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int ret = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+
+            }
+            map.put(s.charAt(i), i);
+            ret = Math.max(ret, i - left + 1);
+        }
+        return ret;
+    }
+}
