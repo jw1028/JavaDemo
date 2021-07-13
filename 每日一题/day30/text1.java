499d72eda0ce577cdc9d9dfb0a3eb2ead79840d0
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-14 07:36:55 +0800

    括号序列

--- 每日一题/day30/text1.java
+++ 每日一题/day30/text1.java
@@ -1,2 +1,11 @@
-dsa
-dsa
+public class Solution {
+
+    public boolean isValid (String s) {
+        // write code here
+        int len = s.length();
+        for(int i = 0; i < len; i++) {
+            s = s.replace("()","").replace("[]","").replace("{}","");
+        }
+        return s.length() == 0;
+    }
+}
