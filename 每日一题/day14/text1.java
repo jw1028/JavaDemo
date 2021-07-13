1830fdcd740a7882d0c2da4cb21c98355f1a33c8
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-13 21:34:11 +0800

    寻找第k大数

--- 每日一题/day14/text1.java
+++ 每日一题/day14/text1.java
@@ -1,1 +1,15 @@
-dsa
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+        for(int x : nums) {
+            minheap.offer(x);
+            if(minheap.size() > k) {
+                minheap.poll();
+            }
+        }
+        return minheap.peek();
+    }
+}
