2e6fa98131584f38dc66b6863bd6c00c727d88a2
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-13 21:07:46 +0800

    两数之和

--- 每日一题/day13/text1.java
+++ 每日一题/day13/text1.java
@@ -1,1 +1,22 @@
-dsa
+import java.util.*;
+
+
+public class Solution {
+
+
+    public int[] twoSum (int[] numbers, int target) {
+        // write code here
+        if(numbers == null || numbers.length == 0) {
+            return null;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < numbers.length; i++) {
+            if(map.containsKey(target - numbers[i])) {
+                return new int[]{map.get(target - numbers[i]) + 1, i + 1};
+//                 return new int[]{i + 1, map.get(target - numbers[i]) + 1};
+            }
+            map.put(numbers[i], i);
+        }
+        return null;
+    }
+}
