dc1813e2c89165a73cc5b8d52db2d8cfb62f16c8
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-13 23:08:47 +0800

    合并两个有序的数组

--- 每日一题/day15/text1.java
+++ 每日一题/day15/text1.java
@@ -1,1 +1,17 @@
-d
+public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+        int p = n + m - 1;
+        m--;
+        n--;
+        while(n >= 0 && m >= 0) {
+            if(A[m] >= B[n]) {
+                A[p--] = A[m--];
+            }else {
+                A[p--] = B[n--];
+            }
+        }
+        while(n >= 0) {
+            A[p--] = B[n--];
+        }
+    }
+}
