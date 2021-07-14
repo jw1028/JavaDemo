554fcef2a42babcf20b24030c663280b17c38e81
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-14 22:55:40 +0800

    股票的最大值

--- 每日一题/day16/text1.java
+++ 每日一题/day16/text1.java
@@ -1,1 +1,16 @@
-dsa
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int res = 0, min = prices[0];
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] <= min) {
+                min = prices[i];
+            }else {
+                res = Math.max(res, prices[i] - min);
+            }
+        }
+        return res;
+    }
+}
