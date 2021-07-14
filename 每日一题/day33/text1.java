a38fa352fc21620a6f5e5330bc5c3ea32416b58c
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-14 22:43:26 +0800

    最长公共前缀

--- 每日一题/day33/text1.java
+++ 每日一题/day33/text1.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+
+public class Solution {
+    public String longestCommonPrefix (String[] strs) {
+        // write code here
+        if(strs == null || strs.length == 0) {
+            return "";
+        }
+        String ret = strs[0];
+        for(int i = 0; i < strs[0].length(); i++) {
+            char ch = ret.charAt(i);
+            for(int j = 1; j < strs.length; j++) {
+                if(i == strs[j].length() || strs[j].charAt(i) != ch) {
+                    return ret.substring(0, i);
+                }
+            }
+        }
+        return ret;
+    }
+}
