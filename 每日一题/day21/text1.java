453cf9cdaa54c39715447c13c2874a737d95a1a6
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-14 08:23:33 +0800

    螺旋矩阵

--- 每日一题/day21/text1.java
+++ 每日一题/day21/text1.java
@@ -1,1 +1,27 @@
-dsa
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return ret;
+        }
+        int n = matrix.length, m = matrix[0].length;
+        boolean[][] arr = new boolean[n][m];
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        int x = 0, y = 0, d = 1;
+        for(int i = 0; i < n * m; i++) {
+            ret.add(matrix[x][y]);
+            arr[x][y] = true;
+            int a = x + dx[d], b = y + dy[d];
+            if(a < 0 || a >= n || b < 0 || b >= m || arr[a][b]) {
+                d = (d + 1) % 4;
+                a = x + dx[d];
+                b = y + dy[d];
+            }
+            x = a;
+            y = b;
+        }
+        return ret;
+    }
+}
