ab641c681eee47cce9521d0f9caeff9801173f87
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-14 22:47:38 +0800

    扑克牌的顺子

--- 每日一题/day27/text1.java
+++ 每日一题/day27/text1.java
@@ -1,1 +1,29 @@
-dsa
+public class Solution {
+    public boolean IsContinuous(int [] numbers) {
+         int max = numbers[0];
+        for(int i=1;i<numbers.length;i++){
+            if(max<numbers[i]){
+                max = numbers[i];
+            }
+        }
+        int min = max;
+        for(int i=0;i<numbers.length;i++){
+            if(min>numbers[i] && numbers[i]!=0){
+                min = numbers[i];
+            }
+        }
+         
+        // 记录每个数字出现的次数，看看有没有重复的数；
+        int count = 0;
+        for(int i=min;i<=max;i++){
+            for(int j=0;j<numbers.length;j++){
+                if(numbers[j] == i && numbers[j]!=0) count++;
+            }
+            if(count>1) return false;
+            count = 0;
+        }
+         
+        if(max-min<5) return true;
+        else return false;
+    }
+}
