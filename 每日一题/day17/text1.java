5eab3b8ae85c1b670f3f4248af6f9dc6a013bea6
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-07-15 10:47:07 +0800

    平衡二叉树

--- 每日一题/day17/text1.java
+++ 每日一题/day17/text1.java
@@ -1,1 +1,20 @@
-asf
+public class Solution {
+    public int high(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int l = high(root.left);
+        int r = high(root.right);
+        if(Math.abs(l - r) <= 1 && l != -1 && r != -1) {
+            return Math.max(l, r) + 1;
+        }else {
+            return -1;
+        }
+    }
+    public boolean IsBalanced_Solution(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        return high(root) >= 0;
+    }
+}
